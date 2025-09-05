c97b72e88f57009d4e78e5d7bea53ee38583e81d
Henrique Lima <riquelima00@gmail.com>
2025-09-05 16:29:05 -0300

    Add TextInput and Button

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Formulário de Usuário</Text>
+      <TextInput style={styles.input}
+      placeholder='Nome'
+      placeholderTextColor='#999999'      
+      />
+      <TextInput style={styles.input}
+      placeholder='Email'
+      placeholderTextColor='#999999'      
+      />
+      <TextInput style={styles.input}
+      placeholder='URL do Avatar'
+      placeholderTextColor='#999999'      
+      />
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.botaoTexto}>Salvar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,7 +28,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  title:{
+    backgroundColor: '#ed145b',
+    width: 350,
+    padding: 10,
+    alignSelf: 'center',
+    color: '#fff',
+    marginBottom: 20,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+
+  input: {
+    borderWidth: 2,
+    borderColor: '#696969',
+    padding: 10,
+    marginBottom: 20,
+    alignSelf: 'center',
+    width: 350,
+  },
+
+  botao: {
+    backgroundColor: '#ed145b',
+    padding: 10,
+    borderRadius: 5,
+    width: 350,
+    alignItems: 'center',
+    alignSelf: 'center'
+  },
+  botaoTexto: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
